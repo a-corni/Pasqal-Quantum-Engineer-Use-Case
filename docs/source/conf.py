13ae29350d66bea2e9ae366ce6de62b016486f1c
a-corni <61453516+a-corni@users.noreply.github.com>
2022-10-01 10:09:03 -0400

    Create conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Pasqal'
+copyright = '2022, Antoine Cornillot'
+author = 'Antoine Cornillot'
 
 release = '0.1'
 version = '0.1.0'
