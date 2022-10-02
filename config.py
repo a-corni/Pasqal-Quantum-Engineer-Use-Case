9c041010106f57c8c0a07543fa1ef453e64e09c8
a-corni <antoine.cornillot@student-cs.fr>
2022-10-02 13:11:04 -0400

    3. Replace oscilloscope by photodiode in config

--- config.py
+++ config.py
@@ -5,50 +5,56 @@ center_freq = 100e6  # 100 MHz
 # Analog output 1 used to send a pulse to the AOM
 analog_output_1 = {1: {'offset': 0.0,
                        'delay': 0.0}}
-# Analog output 2 used to trigger the oscilloscope
-analog_output_2 = {2: {'offset': 0.0,
-                       'delay': 0.0}}
-analog_outputs = {}
-analog_outputs.update(analog_output_1)
-analog_outputs.update(analog_output_2)
+# Analog input 1 used to acquire data from the photo-diode
+analog_input_1 = {1: {'offset': 0.0,
+                      'gain_db': 0.0}}
 
-# Use the Analog Output 1 and 2 ports of an opx2 (has a delay)
+# Use the Analog Output 1 and Analog Input 1 ports of an opx2 (has a delay)
 ctrl_name = "OPX+"
 OPX = {'type': 'opx2',
-       'analog_outputs': analog_outputs}
+       'analog_outputs': analog_output_1,
+       "analog_inputs": analog_input_1}
 controllers = {ctrl_name: OPX}
 
-# Two hardware elements: AOM and oscilloscope
+# Two hardware elements: AOM and photo-diode
 AOM = {"singleInput": {"port": (ctrl_name, 1)},  # AOM is connected to first port of the controller
        "oscillator": "osc",  # Uses oscillator at frequency 100MHz
        "operations": {"amp_mod": "amp_mod_pulse"}}  # Receives 1 pulse
-oscillo = {"singleInput": {"port": (ctrl_name, 2)},  # oscilloscope connected to port 2 of the controller
-           "oscillator": "osc",  # Uses same oscillator
-           "operations": {"trigger": "trigger_pulse"}}  # Receives 1 pulse
+photodiode = {"outputs": {"out1": ("con1", 1)},  # measure on input port 1
+              "oscillator": "osc",  # Uses same oscillator
+              "operations": {"meas": "meas_pulse"}}  # Performs measurement operation
 elements = {"AOM": AOM,
-            "oscillo": oscillo}
+            "photodiode": photodiode}
 
 # Define the pulses
 # AOM RF pulse has length 10us + 120ns
 amp_mod_pulse = {"operation": "control",
                  "length": 10120,
                  "waveforms": {"single": "amp_mod_wf"}}
-# Trigger pulse for oscillo (duration is below the duration of AOM RF pulse)
-trigger_pulse = {"operation": "control",
-                 "length": 9000,
-                 "waveforms": {"single": "trigger_wf"}}
+
+# Measurement pulse for the photodiode
+# Demodulated with cosine
+meas_pulse = {"length": 10000,
+              "operation": "measurement",
+              "integration_weights": {"integration": "integration"}}
 pulses = {"amp_mod_pulse": amp_mod_pulse,
-          "trigger_pulse": trigger_pulse}
+          "meas_pulse": meas_pulse}
 
 # Define the waveforms
 # AOM RF Pulse has amplitude 0.25V
-# Trigger Pulse has amplitude 0.25V (just interested in a square signal)
 amp_mod_wf = {"type": "constant", "sample": 0.25}
-trigger_wf = {"type": "constant", "sample": 0.25}
-waveforms = {"amp_mod_wf": amp_mod_wf,
-             "trigger_wf": trigger_wf}
+waveforms = {"amp_mod_wf": amp_mod_wf}
 
-integration_weights = {}
+# Define the integration weights
+# Want to avoid overflow: integration weights such that:
+# - raw ADC data scaled to between -0.5 and 0.5 multiplied by weights has result between -2 and 2.
+# - The sum of the multiplication above is smaller than 2**16
+# If we assume constant weights w=1, and a constant signal a[n] = a*cos(\omega_{IF} t_s n)
+# then result is d = \frac{a N cos(\phi) 2{-12}}{2}
+# The signal is constant and not modulated. We perform the average by integration during 10 \mu s
+# Weights used for integration are defined in cosine.
+integration_weights = {"integration": {"cosine": [(1/10000, 10000)],
+                                       "sine": [(0.0, 10000)]}}
 mixers = {}
 
 # Frequency oscillation 100MHz
